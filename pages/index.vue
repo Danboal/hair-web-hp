86ab7933cb77d525c3cde7d96783f1af4b133755
Danboal <48382552+Danboal@users.noreply.github.com>
2023-09-08 16:17:13 +0900

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,28 @@
 <template>
-  <v-container style="height:1000px">
-    <v-row no-gutters>
+  <v-container>
+    <v-row>
       <v-col>
         <Logo />
       </v-col>
     </v-row>
-    <v-row no-gutters>
+    <v-row>
       <v-col>
-        <div>
-          <div class="text-sm-h3 text-h4">理念とか</div>
-          何か書く　SNS埋め込みはこのページ
-          SEOIndexもこのページ
-        </div>
+        <Instagram />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <GoogleMap />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <Hotpepper />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <Contact />
       </v-col>
     </v-row>
   </v-container>
