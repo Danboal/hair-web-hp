48469a9b22c435a5f53c9c21e540ec5a5d5120ce
Danboal <48382552+Danboal@users.noreply.github.com>
2023-09-14 09:48:19 +0900

    Update Contact.vue

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,10 +1,45 @@
 <template>
     <div>
         <div class="text-sm-h3 text-h4">Contact us</div>
-        <form>
+        <v-form v-model="valid">
             <v-text-field
+              v-model="name"
+              :rules="nameRules"
+              label="お名前"
+              required
             ></v-text-field>
-        </form>
+            <v-text-field
+              v-model="email"
+              :rules="nameRules"
+              label="メールアドレス"
+              required
+            ></v-text-field>
+            <v-select
+              :items="items"
+              label="ご用件"
+              outlined
+              required
+            ></v-select>
+            <v-textarea
+            outlined
+            v-model="detail"
+            label="お問合せ内容"
+          ></v-textarea>
+          <v-checkbox
+            v-model="checkbox"
+            :rules="[v => !!v || 'You must agree to continue!']"
+            label="個人情報取扱に同意します"
+            required
+          ></v-checkbox>
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="validate"
+        >
+            送信
+        </v-btn>
+        </v-form>
     </div>
 </template>
 
