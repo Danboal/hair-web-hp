ed08837065deb26b0eb1c57517b1b475aabf5ba2
Danboal <48382552+Danboal@users.noreply.github.com>
2023-09-14 09:49:12 +0900

    Update Hotpepper.vue

--- components/Hotpepper.vue
+++ components/Hotpepper.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="text-sm-h3 text-h4">HotPepper</div>
-        <div><v-img src="Beauty_Logo_270_90.gif" href="https://beauty.hotpepper.jp/" target="blank"></v-img></div>
+        <div><a href="https://beauty.hotpepper.jp/CSP/bt/reserve/?storeId=H000665254" target="blank"><v-img src="Beauty_Logo_270_90.gif"></v-img></a></div>
     </div>
 </template>
 
