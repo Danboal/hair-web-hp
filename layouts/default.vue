00a42299f8f5ee77b9160d5b0d6d85396f2be2e1
Danboal <www.masaya1@gmail.com>
2023-09-14 21:28:47 +0900

    change

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-app dark>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      dense
-      elevate-on-scroll
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }} - Dalahast</span>
-    </v-footer>
-  </v-app>
+  <transition>
+    <v-app dark>
+      <v-app-bar
+        :clipped-left="clipped"
+        fixed
+        dense
+        elevate-on-scroll
+        app
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <Nuxt />
+        </v-container>
+      </v-main>
+      <v-footer
+        :absolute="!fixed"
+        app
+      >
+        <span>&copy; {{ new Date().getFullYear() }} - Dalahast</span>
+      </v-footer>
+    </v-app>
+  </transition>
 </template>
 
 <script>
